dff1d0194698052655212cc90979b0882d84b4d6
Mark Skipper <marks@aptivate.org>
2014-11-10 22:15:44 +0000

    Add BDD tests with behave

---
+++ bdd/features/steps/mylist.py
@@ -0,0 +1,16 @@
+from behave import given, when, then
+
+
+@given('a user')
+def null(context):
+    pass
+
+
+@when('a user visits the site')
+def visit(context):
+    context.browser.visit(context.home_url)
+
+
+@then('the page title contins \'{text}\'')
+def title_content(context, text):
+    assert text in context.browser.title.decode()
