881d01d5da25c0b52e20eca39501877d2aaa765b
Mark Skipper <marks@aptivate.org>
2014-11-11 23:23:55 +0000

    Move bdd tests to features dir in app

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_behave',
+    'superlists',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,3 +83,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEST_RUNNER = 'django_behave.runner.DjangoBehaveTestSuiteRunner'
