2edf5720044261e7273877307e1bee2b2c59e72b
Mark Skipper <marks@aptivate.org>
2015-03-09 23:07:09 +0000

    Redirect after a POST
    Change test to expect a redirect (302) and
    make the view redirect after a POST

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render,
+    redirect
+)
 
 from .models import Item
 
 
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST.get('item_text', '')
-        Item.objects.create(text=new_item_text)
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
     else:
-        new_item_text = ''
-
-    return render(request, 'home.html', {
-        'new_item_text': new_item_text
-    })
+        return render(request, 'home.html')
