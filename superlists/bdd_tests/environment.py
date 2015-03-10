ba5af83820ec51adff502acaea1ee872ad90176f
Mark Skipper <marks@aptivate.org>
2015-03-10 00:04:50 +0000

    Add the counter variable and make tests pass
    Making them pass entails cleaning the database between
    scenarios. Doing this by running the manage command from
    python at the moment, which probably isn't as fast as
    it would be to use LiveServerTestCae properly.

--- superlists/bdd_tests/environment.py
+++ superlists/bdd_tests/environment.py
@@ -1,5 +1,6 @@
 from splinter import Browser
 from unittest import TestCase
+from django.core.management import call_command
 
 
 print("env")
@@ -13,3 +14,9 @@ def before_all(context):
 
 def after_all(context):
     context.browser.quit()
+
+def before_scenario(context, feature):
+    """ Run each scenario in a separate database copy """
+    # This is a hack, we really should use transactions and roll back
+    # See https://github.com/django-behave/django-behave/issues/43
+    call_command('flush', verbosity=0, interactive=False)
