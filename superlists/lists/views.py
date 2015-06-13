808c6f813ea54c0cb59797bddf3f82d9310747f9
Mark Skipper <marks@aptivate.org>
2015-06-13 21:27:47 +0100

    Add a new URL for viweing liss
    New Template for new url
    and separate out viewing-existing from starting new funcionality.

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -11,18 +11,13 @@ def home_page(request):
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/lists/unique-list/')
     else:
-        items = Item.objects.all()
-        return render(
-            request,
-            'home.html',
-            {'items': items}
-        )
+        return render(request, 'home.html')
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(
         request,
-        'home.html',
+        'list.html',
         {'items': items}
     )
